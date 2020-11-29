fe206a9baf3f25a6b81bdd6439d469620331cb63
“lkeeee” <finalfantasyx15@hotmail.com>
2020-11-29 23:57:23 +1100

    added 3 timeouts to the integration tests

--- src/test/integration/user-creation.test.js
+++ src/test/integration/user-creation.test.js
@@ -22,16 +22,19 @@ describe('user creation page', function () {
   });
 
   it('loads correctly', function (done) {
+    this.timeout(60000);
     request(app).get('/').expect(200, done);
   });
 
   it('lists a user if there is one', function (done) {
+    this.timeout(60000);
     this.models.Users.create({ username: 'johndoe' }).then(function () {
       request(app).get('/').expect(/johndoe/, done);
     })
   });
 
   it('lists the tickets for the user if available', function (done) {
+    this.timeout(60000);
     this.models.Users.create({ username: 'johndoe' }).bind(this).then(function (user) {
       return this.models.Tasks.create({ title: 'johndoe task', UserId: user.id });
     }).then(function () {
